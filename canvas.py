3353a6c342a569801d99fce777d0c18b1661553a
Mustiskooo <110821966+Mustiskooo@users.noreply.github.com>
2026-07-29 15:17:10 +0300

    feat: canvas
    Implement an infinite canvas using QGraphicsView with zoom and scroll functionalities.

---
+++ canvas.py
@@ -0,0 +1,50 @@
+from PySide6.QtCore import Qt
+from PySide6.QtGui import QBrush, QColor, QPainter
+from PySide6.QtWidgets import QGraphicsScene, QGraphicsView
+
+
+class InfiniteCanvas(QGraphicsView):
+    def __init__(self):
+        super().__init__()
+
+        self.scene = QGraphicsScene(self)
+        self.setScene(self.scene)
+
+#sonsuz hissi için büyük alan
+        self.scene.setSceneRect(-10000, -10000, 20000, 20000)
+
+#canvas rengi
+        self.setBackgroundBrush(QBrush(QColor("#FCFCFC")))
+#render
+        self.setRenderHint(QPainter.Antialiasing)
+#scrollbar gizleme
+        self.setHorizontalScrollBarPolicy(
+            Qt.ScrollBarAlwaysOff
+        )
+        self.setVerticalScrollBarPolicy(
+            Qt.ScrollBarAlwaysOff
+        )
+#pan
+        self.setDragMode(
+            QGraphicsView.ScrollHandDrag
+        )
+#zoom seviyesi
+        self.zoom = 1.0
+
+
+    def wheelEvent(self, event):
+        zoom_factor = 1.15
+
+        if event.angleDelta().y() > 0:
+            self.zoom *= zoom_factor
+            self.scale(
+                zoom_factor,
+                zoom_factor
+            )
+
+        else:
+            self.zoom /= zoom_factor
+            self.scale(
+                1 / zoom_factor,
+                1 / zoom_factor
+            )
